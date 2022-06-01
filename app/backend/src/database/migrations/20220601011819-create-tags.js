23ed16f81670920929cce5c81a244aec1debc4a6
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-06-01 08:13:00 -0400

    :sparkles: Feat(ORM): Initial configs ... Created .sequelizerc, devices and tags (migrations/models) to begin tables mapping.

---
+++ app/backend/src/database/migrations/20220601011819-create-tags.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      color: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('tags');
+  }
+};
