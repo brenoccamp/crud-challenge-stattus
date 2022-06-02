d43fcf3725bf963f3aebccc71d3de467bea8924e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-06-02 13:10:29 -0400

    :white_check_mark: Test(Unit Tests): Created unit tests of layers service ... Implemented Unit tests of layers service of DeviceService.ts and TagService.ts.

--- app/backend/src/tests/_mocks_/unit_mocks/tagsMocks.ts
+++ app/backend/src/tests/_mocks_/unit_mocks/tagsMocks.ts
@@ -1,34 +1,47 @@
 export const mockedAllTags = [
   {
-      "id": 1,
-      "name": "São Paulo",
-      "color": "#000"
+    "id": 1,
+    "name": "São Paulo",
+    "color": "#000"
   },
   {
-      "id": 2,
-      "name": "Sorocaba",
-      "color": "#fff"
+    "id": 2,
+    "name": "Sorocaba",
+    "color": "#fff"
   },
   {
-      "id": 3,
-      "name": "Rio de Janeiro",
-      "color": "#303030"
+    "id": 3,
+    "name": "Rio de Janeiro",
+    "color": "#303030"
   },
   {
-      "id": 4,
-      "name": "Angra",
-      "color": "#313131"
+    "id": 4,
+    "name": "Angra",
+    "color": "#313131"
   },
   {
-      "id": 5,
-      "name": "Minas Gerais",
-      "color": "#414141"
+    "id": 5,
+    "name": "Minas Gerais",
+    "color": "#414141"
   },
   {
-      "id": 6,
-      "name": "Belo Horizonte",
-      "color": "#515151"
+    "id": 6,
+    "name": "Belo Horizonte",
+    "color": "#515151"
   }
-]
+];
+
+export const mockedCreatedTag = {
+  id: 1,
+  name: "São Paulo",
+  color: "#000",
+  updatedAt: '2022-06-01T18:20:33.829Z',
+  createdAt: '2022-06-01T18:20:33.829Z'
+}
+
+export const mockedNewTagData = {
+  name: 'São Paulo',
+  color: '#000'
+}
 
 export const mockedCreatedNewTagID = { createdId: 1 };
