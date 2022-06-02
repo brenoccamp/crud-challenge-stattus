8954fd57213aaea6b7739c95ded20de839e779e4
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-06-02 17:39:04 -0400

    :sparkles: Feat(EDIT, UPDATE, DELETE TAG): Created methods patch, put, delete ... Implemented methods editTag, updateTag, deleteTag on files TagController.ts and TagService.ts. Also implemented middlewares to validate all actions.

--- app/backend/src/interfaces/tagInterfaces.ts
+++ app/backend/src/interfaces/tagInterfaces.ts
@@ -10,10 +10,16 @@ export interface ITagController {
   getAllTags(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   getTagById(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   createNewTag(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  editTag(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  updateTag(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  deleteTag(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
 }
 
 export interface ITagService {
   getAllTags(): Promise<ITag[]>;
   getTagById(id: number): Promise<ITag | null>;
   createNewTag(newTag: ITag): Promise<{ createdId: number }>;
+  editTag(id: number, newData: string, fieldToEdit: string): Promise<boolean>;
+  updateTag(id: number, newData: ITag): Promise<boolean>;
+  deleteTag(id: number): Promise<boolean>;
 }
